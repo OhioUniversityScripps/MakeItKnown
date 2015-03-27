9aea4408f31bc2e9eeb3dc98681253e58fb990e4
Ricky Chilcott <ricky@rbsavvy.com>
2015-03-27 18:41:07 -0400

    Rip out jQuery (who needs it?), reduce external resource calls, and speed things up (by about 10X)

--- makeitso.js
+++ makeitso.js
@@ -1,23 +1,85 @@
-$(function() {
+window.onload = function () {
   setBackground();
   setStatement();
-});
+}
 
 setStatement = function(){
-  statement = $('#statement');
+  statement = document.getElementById('statement');
 
-  statement.text(randomItem(window.statements));
-  return true;
+  return statement.innerText = randomItem(window.statements);
 }
 
 setBackground = function(){
-  $('body').css('background-color', randomItem(window.backgrounds));
-  console.log
-  return true;
+  return document.body.style.background = randomItem(window.backgrounds);
 }
 
 randomItem = function(array) {
-  index = Math.floor(Math.random() * array.length);
-
-  return array[index];
+  return array[Math.floor(Math.random() * array.length)];
 }
+
+window.statements = [
+  "true",
+  "believable",
+  "understood",
+  "important",
+  "essential",
+  "acessible",
+  "personal",
+  "universal",
+  "yours",
+  "accepted",
+  "necessary",
+  "matter",
+  "reach",
+  "connect",
+  "click",
+  "relevenat",
+  "reasonable",
+  "undeniable",
+  "felt",
+  "move",
+  "persuasive",
+  "authoritative",
+  "direct",
+  "pure",
+  "pivotal",
+  "certain",
+  "so",
+  "real",
+  "revolutinary",
+  "controversial",
+  "rational",
+  "urgent",
+  "vital",
+  "sing",
+  "speak",
+  "serious",
+  "funny",
+  "insightful",
+  "incisive",
+  "decisive",
+  "loud",
+  "clear",
+  "known"
+];
+
+window.backgrounds = [
+  "#9C8F30",
+  "#69913B",
+  "#8FD400",
+  "#1F3730",
+  "#003050",
+  "#6EB4CD",
+  "#007396",
+  "#872434",
+  "#C0143C",
+  "#673BB8",
+  "#612D62",
+  "#F4AA00",
+  "#EF8200",
+  "#DF8D25",
+  "#5E4F4A",
+  "#6F4C23",
+  "#57655F",
+  "#9D986D"
+];
